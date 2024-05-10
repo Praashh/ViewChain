9435aff28dba43e47eb65165e6fd283970c01b49
Prashant Varma <prashantvarma5083@gmail.com>
2024-05-10 12:35:59 +0530

    testing ci pipeline

--- app/(lobby)/room/[id]/page.tsx
+++ app/(lobby)/room/[id]/page.tsx
@@ -4,5 +4,9 @@ import { useParams } from 'next/navigation'
 export default function Page() {
   const router = useParams()
   console.log(router)
-  return <p className='text-white'>Post: {router.id}</p>
+  return (
+    <>
+      ID:{router.id}
+    </>
+  )
 }
