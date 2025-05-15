d5bdc2107bd66800feb372d4b23cb76c195a7ec7
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2025-05-16 02:15:42 +0530

    feat: landing revamped

--- apps/client/components/ui/avtar-circle.tsx
+++ apps/client/components/ui/avtar-circle.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 import {
   Dialog,
   DialogTrigger,
@@ -11,14 +11,14 @@ import {
   DialogHeader,
   DialogTitle,
   DialogDescription,
-  DialogFooter
-} from '@/components/ui/dialog';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { signIn } from 'next-auth/react';
-import { toast } from 'sonner';
-import { useAuth } from '@/hooks/useAuth';
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { toast } from "sonner";
+import { useAuth } from "@/hooks/useAuth";
 
 type Avatar = {
   img: string;
@@ -34,88 +34,92 @@ interface AvatarCirclesProps {
 const AvatarCircles = ({
   numPeople,
   className,
-  avatarUrls
+  avatarUrls,
 }: AvatarCirclesProps) => {
-  const [passCode, setPassCode] = useState<string>('');
+  const [passCode, setPassCode] = useState<string>("");
   const [loader, setLoader] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false); // control dialog manually
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   async function handleLogin() {
     setLoader(true);
 
-    if (
-      passCode === process.env.NEXT_PUBLIC_SUPER_LOGIN_PASS_KEY as string
-    ) {
-      await signIn('credentials', {
+    if (passCode === (process.env.NEXT_PUBLIC_SUPER_LOGIN_PASS_KEY as string)) {
+      await signIn("credentials", {
         email: process.env.NEXT_PUBLIC_SUPER_LOGIN_ID as string,
-        callbackUrl: '/marketplace'
+        callbackUrl: "/marketplace",
       });
       // No need to close dialog manually, redirect will happen
     } else {
-      toast.error('Wrong Key Entered!');
+      toast.error("Wrong Key Entered!");
       setLoader(false);
     }
   }
 
   return (
-    <div className={cn('z-10 flex -space-x-4 rtl:space-x-reverse', className)}>
-      {avatarUrls.map((item, index) =>
-        item.hkirat && !user?.email ?  (
-          <Dialog key={index} open={open} onOpenChange={setOpen}>
+    <div
+      className={cn(
+        "z-10 flex border bg-muted/70 backdrop-blur-md rounded-lg flex-col rtl:space-x-reverse",
+        className
+      )}
+    >
+      <div className="flex p-1 items-center gap-1">
+        {avatarUrls.map((item, index) =>
+          item.hkirat && !user?.email ? (
+            <Dialog key={index} open={open} onOpenChange={setOpen}>
+              <img
+                className="size-10 cursor-pointer rounded-md shadow-xl"
+                src={item.img}
+                width={40}
+                height={40}
+                alt={`Avatar ${index + 1}`}
+                onDoubleClick={() => setOpen(true)}
+              />
+
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Super Special Login</DialogTitle>
+                  <DialogDescription>
+                    Please enter the super special login code to login.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="login" className="text-right">
+                      Login Code
+                    </Label>
+                    <Input
+                      type="password"
+                      className="col-span-3"
+                      onChange={(e) => setPassCode(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit" onClick={handleLogin} disabled={loader}>
+                    {loader ? "Signing..." : "Signin"}
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          ) : (
             <img
-              className="h-10 w-10 cursor-pointer rounded-full shadow-xl"
+              key={index}
+              className="size-10 cursor-pointer rounded-md shadow-xl"
               src={item.img}
               width={40}
               height={40}
               alt={`Avatar ${index + 1}`}
-              onDoubleClick={() => setOpen(true)}
             />
-
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Super Special Login</DialogTitle>
-                <DialogDescription>
-                  Please enter the super special login code to login.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="login" className="text-right">
-                    Login Code
-                  </Label>
-                  <Input
-                    type="password"
-                    className="col-span-3"
-                    onChange={(e) => setPassCode(e.target.value)}
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <Button
-                  type="submit"
-                  onClick={handleLogin}
-                  disabled={loader}
-                >
-                  {loader ? 'Signing...' : 'Signin'}
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        ) : (
-          <img
-            key={index}
-            className="h-10 w-10 cursor-pointer rounded-full shadow-xl"
-            src={item.img}
-            width={40}
-            height={40}
-            alt={`Avatar ${index + 1}`}
-          />
-        )
-      )}
-      <span className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-primary bg-primary text-center text-xs font-medium text-white dark:border-primary dark:bg-primary dark:text-black">
-        +{numPeople}
-      </span>
+          )
+        )}
+        <span className="flex size-10 items-center justify-center rounded-md font-semibold border-2 border-primary bg-primary text-center text-xs drop-shadow-2xl text-white dark:border-primary dark:bg-primary dark:text-black">
+          +{numPeople}
+        </span>
+      </div>
+      <div className="size-6 text-xs w-full rounded-b-lg text-center bg-accent/10 backdrop-blur-md p-1">
+        Loved by users
+      </div>
     </div>
   );
 };
--- apps/client/components/ui/main-nav.tsx
+++ apps/client/components/ui/main-nav.tsx
@@ -1,40 +1,69 @@
-"use client"
-import { View } from 'lucide-react';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-import { Button } from './button';
-import useCheckUserBoarded from '@/hooks/useCheckUserBoarded';
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Button } from "./button";
+import useCheckUserBoarded from "@/hooks/useCheckUserBoarded";
+import { Logo } from "../svgs/logo";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
-    const { data: session } = useSession();
-   const {callbackUrl} = useCheckUserBoarded();
-    
-    return (
-    <div>
-         <header className=" text-white px-4 lg:px-6 py-4 flex items-center justify-between">
+  const { data: session } = useSession();
+  const { callbackUrl } = useCheckUserBoarded();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 20;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrolled]);
+
+  return (
+    <div
+      className={cn(
+        "fixed top-4 px-4 left-1/2 -translate-x-1/2 w-full z-[40] transition-all duration-300 ease-in-out",
+        scrolled ? "max-w-4xl top-8 pt-0" : "max-w-5xl pt-4"
+      )}
+    >
+      <header
+        className={cn(
+          "px-2 py-2 flex items-center justify-between rounded-lg transition-all duration-300 ease-in-out",
+          scrolled ? "bg-accent/40 backdrop-blur-md" : "bg-transparent"
+        )}
+      >
         <Link className="flex items-center gap-2" href="#">
-          <View className="h-6 w-6" />
-          <span className="text-xl font-bold">ViewChain</span>
+          <Logo className="size-8" />
+          <span className="text-xl font-medium">ViewChain</span>
         </Link>
-        {
-          session ? (
-            <Button className="hidden lg:inline-flex" variant="secondary"
-            onClick={() => signOut()}
-          >
-            Logout
-          </Button>
+        <div className="flex items-center gap-2">
+          <Button variant={"secondary"}>Contact</Button>
+          {session ? (
+            <Button
+              className="inline-flex"
+              variant="destructive"
+              onClick={() => signOut()}
+            >
+              Logout
+            </Button>
           ) : (
-            <Button className="hidden lg:inline-flex" variant="secondary"
-              onClick={() => signIn("google", { callbackUrl})}
+            <Button
+              className=""
+              variant="default"
+              onClick={() => signIn("google", { callbackUrl })}
             >
               Signin
             </Button>
-          )
-        }
+          )}
+        </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
